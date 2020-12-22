86b2047e0165c8f7c46129a73ef7069982264e9c
semiwaker <semiwaker@163.com>
2020-12-22 20:10:58 +0800

    skip_frame

--- client_utility.py
+++ client_utility.py
@@ -1,16 +1,17 @@
 import asyncio
+import math
 import threading
+import time
 
 
 class MediaBuffer:
     def __init__(self, interval, max_size, fetch_size):
         self.buf = [{'timestamp': 0, 'data': None}]
-        self.cur = 1
+        self.cur = 0
         self.max_size = max_size
         self.interval = interval
         self.fetch_size = fetch_size
         self.file_name = ""
-        self.last_data = None
 
         self.buf_lock = threading.Lock()
         self.cv = threading.Condition(self.buf_lock)
@@ -22,6 +23,7 @@ class MediaBuffer:
         self.file_name = file_name
 
     async def open(self):
+        # TODO: send open
         pass
 
     def insert(self, timestamp, data):
@@ -48,12 +50,14 @@ class MediaBuffer:
     def fetch(self):
         with self.cv:
             self.cv.wait_for(self.available)
-            data = self.buf[self.cur]['data']
-            if data is None:
-                data = self.last_data
-            else:
-                self.last_data = data
-            self.cur = self.cur + 1
+
+            data = None
+            while data is None:
+                self.cur = self.cur + 1
+                data = self.buf[self.cur]['data']
+                if data is None:
+                    threading.sleep(self.interval)
+
             if self.cur > 2*self.fetch_size:
                 self.cur -= self.fetch_size
                 self.buf = self.buf[self.fetch_size:]
